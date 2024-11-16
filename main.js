4ee1ff957e71e816f011fdc61c8fbfdeb9cec6de
Kacper Fornalczyk <fk46509@zut.edu.pl>
2024-11-16 16:09:43 +0100

    Zmiany po zajeciach

--- main.js
+++ main.js
@@ -5,32 +5,32 @@ const dots = [
 ];
 
 const scenarios = [
-    ["ringSmall", "ringContrast1", "#0029e0", "112px", "232px", 1],
-    ["ringMedium", "ringContrast1", "#0029e0", "57px", "193px", 1],
-    ["ringBig", "ringContrast1", "#0029e0", "2px", "154px", 1],
-    ["ringSmall", "ringContrast2", "#9c9c9c", "112px", "232px", 1],
-    ["ringMedium", "ringContrast2", "#9c9c9c", "57px", "193px", 1],
-    ["ringBig", "ringContrast2", "#9c9c9c", "2px", "154px",  1],
+    ["ringSmall", "ringContrast1", "#0000ff", "112px", "232px", 1],
+    ["ringMedium", "ringContrast1", "#0000ff", "57px", "193px", 1],
+    ["ringBig", "ringContrast1", "##0000ff", "2px", "154px", 1],
+    ["ringSmall", "ringContrast2", "#ff0000", "112px", "232px", 1],
+    ["ringMedium", "ringContrast2", "#ff0000", "57px", "193px", 1],
+    ["ringBig", "ringContrast2", "#ff0000", "2px", "154px",  1],
     ["ringSmall", "ringContrast3", "#ffff00", "112px", "232px",  1],
     ["ringMedium", "ringContrast3", "#ffff00", "57px", "193px",  1],
     ["ringBig", "ringContrast3", "#ffff00", "2px", "154px",  1],
     
-    ["ringSmall", "ringContrast1", "#0029e0", "112px", "232px", 2],
-    ["ringMedium", "ringContrast1", "#0029e0", "57px", "193px", 2],
-    ["ringBig", "ringContrast1", "#0029e0", "2px", "154px", 2],
-    ["ringSmall", "ringContrast2", "#9c9c9c", "112px", "232px", 2],
-    ["ringMedium", "ringContrast2", "#9c9c9c", "57px", "193px", 2],
-    ["ringBig", "ringContrast2", "#9c9c9c", "2px", "154px", 2],
+    ["ringSmall", "ringContrast1", "#7b00ff", "112px", "232px", 2],
+    ["ringMedium", "ringContrast1", "#7b00ff", "57px", "193px", 2],
+    ["ringBig", "ringContrast1", "#7b00ff", "2px", "154px", 2],
+    ["ringSmall", "ringContrast2", "#ff0000", "112px", "232px", 2],
+    ["ringMedium", "ringContrast2", "#ff0000", "57px", "193px", 2],
+    ["ringBig", "ringContrast2", "#ff0000", "2px", "154px", 2],
     ["ringSmall", "ringContrast3", "#ffff00", "112px", "232px", 2],
     ["ringMedium", "ringContrast3", "#ffff00", "57px", "193px", 2],
     ["ringBig", "ringContrast3", "#ffff00", "2px", "154px", 2],
 
-    ["ringSmall", "ringContrast1", "#2B00FF", "112px", "232px", 4],
-    ["ringMedium", "ringContrast1", "#2B00FF", "57px", "193px", 4],
-    ["ringBig", "ringContrast1", "#2B00FF", "2px", "154px", 4],
-    ["ringSmall", "ringContrast2", "#9c9c9c", "112px", "232px", 4],
-    ["ringMedium", "ringContrast2", "#9c9c9c", "57px", "193px", 4],
-    ["ringBig", "ringContrast2", "#9c9c9c", "2px", "154px", 4],
+    ["ringSmall", "ringContrast1", "#7b00ff", "112px", "232px", 4],
+    ["ringMedium", "ringContrast1", "#7b00ff", "57px", "193px", 4],
+    ["ringBig", "ringContrast1", "##7b00ff", "2px", "154px", 4],
+    ["ringSmall", "ringContrast2", "#ff0000", "112px", "232px", 4],
+    ["ringMedium", "ringContrast2", "#ff0000", "57px", "193px", 4],
+    ["ringBig", "ringContrast2", "#ff0000", "2px", "154px", 4],
     ["ringSmall", "ringContrast3", "#ffff00", "112px", "232px", 4],
     ["ringMedium", "ringContrast3", "#ffff00", "57px", "193px", 4],
     ["ringBig", "ringContrast3", "#ffff00", "2px", "154px", 4],
@@ -40,6 +40,69 @@ let scenario = 0;
 let blinkingIntervalId;
 let selected_dot;
 //console.time('myTimer')
+let selectedDot;
+let highlightedDots = [];
+let answeredQuestions = [];
+
+document.getElementById('showCircleBtn').addEventListener('click', function() {
+    // Znajdujemy kontener z okręgiem
+    var container = document.getElementById('container');
+    
+    // Ustawiamy styl display na 'block', aby pokazać ukryty kontener
+    container.style.display = 'block';
+	
+	// Usuwamy przycisk oraz instrukcje startowa po kliknięciu
+	this.remove();
+	document.getElementById('instruction').remove();
+
+    // Wystartowanie z pierwszym scenariuszem
+    nextScenario();
+});
+
+function showNextQuestion() {
+    if (answeredQuestions.length >= 27) {
+        alert("Quiz zakończony!");
+        return;
+    }
+
+    const questionNumber = Math.floor(Math.random() * 128) + 1;
+    document.getElementById('questionImage').src = `images/ciekawostka_${String(questionNumber).padStart(3, '0')}.png`;
+    
+    displayAnswers(questionNumber);
+    // highlightedDots = [];
+
+    // setTimeout(() => {
+    //     displayAnswers(questionNumber);
+    // }, 1000);
+    //askAdditionalQuestion();
+}
+
+function displayAnswers(questionNumber) {
+    document.getElementById('answers').style.display = 'flex';
+    document.getElementById('answerA').src = `images/ciekawostka_${String(questionNumber).padStart(3, '0')}_A.png`;
+    document.getElementById('answerB').src = `images/ciekawostka_${String(questionNumber).padStart(3, '0')}_B.png`;
+    document.getElementById('answerC').src = `images/ciekawostka_${String(questionNumber).padStart(3, '0')}_C.png`;
+    document.getElementById('answerD').src = `images/ciekawostka_${String(questionNumber).padStart(3, '0')}_D.png`;
+}
+
+function selectAnswer(answer) {
+    answeredQuestions.push({ questionNumber: answeredQuestions.length + 1, answer });
+    document.getElementById('answers').style.display = 'none';
+    nextScenario();
+}
+
+function askAdditionalQuestion(questionNumber) {
+    const userResponse = prompt("Ile kropek było podświetlonych?");
+    const correctAnswer = highlightedDots.length;
+
+    answeredQuestions.push({
+        questionNumber,
+        additionalQuestion: "Ile kropek było podświetlonych?",
+        userResponse: userResponse,
+        correctAnswer: correctAnswer
+    });
+}
+
 
 // Podpięcie funkcji anonimowej, uruchamiajacej eksperyment i usuwajacej elemnty startowe
 document.getElementById('showCircleBtn').addEventListener('click', function() {
@@ -61,7 +124,7 @@ document.getElementById('showCircleBtn').addEventListener('click', function() {
 // Funkcja przechodzaca do kolejnego scenariusza
 function nextScenario() {
     //console.log(scenarios[scenario]);
-    
+    console.log("git");
     // Ustawia klas dla okręgu
     var ring = document.getElementById('ring');
     if (scenario > 0) {
@@ -76,7 +139,7 @@ function nextScenario() {
     var circles = document.querySelectorAll('.circle');
     circles.forEach(circle => {
         circle.style.backgroundColor = scenarios[scenario][2];
-        circle.style.opacity = "50%";
+        circle.style.opacity = "0%";
     });
 
     circles[0].style.top = scenarios[scenario][3];
@@ -95,7 +158,7 @@ function nextScenario() {
     
     // Uruchomienie funkcji która ma mrygać
 	blinking(scenarios[scenario][5]);
-    
+    showNextQuestion()
     scenario += 1;
 
     if (scenario == scenarios.length) {
